e0cf781ab59754384a15c33bffedc7f56b72ec36
Dustin Aldana <71577541+D-Aldana@users.noreply.github.com>
2023-03-12 20:20:28 -0700

    Reverted amqplib changes cause nothing fricking works help

--- timo-web-interface/src/components/FileUpload.js
+++ timo-web-interface/src/components/FileUpload.js
@@ -2,30 +2,10 @@ import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
 import { useState } from "react"
 
-const ampq = require('amqplib/callback_api');
 
 const sendOperationFile = (msg) => {
   //TODO: Add connection to Rabbit
-  ampq.connect('connection here', (err, connection) => {
-    if (err) {
-      throw err;
-    }
-    connection.createChannel((err, channel) => {
-      if (err) {
-        throw err;
-      }
-
-      // TODO: Change queue name
-      let queueName = "operation_queue";
-
-      channel.assertQueue(queueName,  {
-        durable: false
-      });
-
-      channel.sendToQueue(queueName, Buffer.from(msg));
-    })
-  }
-  )
+  
 }
 
 function FileUpload(props) {
@@ -39,7 +19,7 @@ function FileUpload(props) {
       reader.readAsText(selectedFile);
       reader.onload = () => {
         console.log(reader.result);
-        sendOperationFile(reader.result)
+        // sendOperationFile(reader.result)
       };
     }
   };
