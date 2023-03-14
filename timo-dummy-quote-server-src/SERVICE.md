55ea7eb82f5ce9606e0a9e33f02eb6151f6334d2
Cameron Ewing <59128151+c-ewing@users.noreply.github.com>
2023-03-14 14:29:43 -0700

    Create Dummy Quote Server:
    Create a dummy test server for testing the quote driver and other quote
    server related microservices. Written as a simple python socket server
    with no error handling or other niceties.
    
    Listens on localhost:4444

---
+++ timo-dummy-quote-server-src/SERVICE.md
@@ -0,0 +1,4 @@
+# Dummy Quote server:
+Create a dummy quote server that listens on localhost:4444 for debugging locally and not in the lab.
+
+Behaves similarly to the lab quote server however it returns the same crpyokey every time and randomly generates the quote price. Always responses with the calling user and the associated stock symbol.
---
+++ timo-dummy-quote-server-src/server.py
@@ -0,0 +1,36 @@
+import socketserver
+import random
+import time
+
+class QuoteServerHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+
+        call = self.data.decode("utf-8").split(",")
+
+        # Generate the response and send it back to the client
+        random_price = random.randint(50, 500) / 100.0
+        response = "%f,%s,%s,%s,%s".format(random_price, call[0], call[1], time.time_ns() / 1000, "Tnssjq2UKzc+KQ/KhjmENlfJSHRD7VBXxiYh1CVpyDo=")
+        # {price,symbol,username,timestamp,cryptokey}
+        print(response)
+        self.request.sendall(response)
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 4444
+
+    # Create the server, binding to localhost on port 9999
+    with socketserver.TCPServer((HOST, PORT), QuoteServerHandler) as server:
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
